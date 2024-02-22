a12b6f3d9f1f1cabfcd2077b0ca32ddae93830ba
Matthew Mayer <grafixgeek@yahoo.com>
2024-02-22 09:49:06 -0500

    Server updated

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,17 +1,22 @@
 import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    // origin: (origin, callback) => {
+    //     if (allowedOrigins.indexOf(origin) !== -1) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error('Not allowed by CORS'));
+    //     }
+    // },
+    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+    // credentials: true, // Allow credentials
+    // origin: ['https://playhouseacademy.onrender.com', 'https://playhouseacademy.onrender.com/backstage']
+    
+    origin: "*", // Allow requests from any origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Allow credentials
-    origin: ['https://playhouseacademy.onrender.com', 'https://playhouseacademy.onrender.com/backstage']
+    credentials: false, // Do not allow credentials
 };
 
 export default corsOptions;
