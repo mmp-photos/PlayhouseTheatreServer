c38b861c676198c0caee7faee98d2b967acb0933
Matthew Mayer <grafixgeek@yahoo.com>
2024-02-22 00:24:58 -0500

    Updating CORS options

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,16 +2,15 @@ import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions = {
     origin: (origin, callback) => {
-        console.log(origin)
-        if (allowedOrigins.indexOf(origin) == -1 || !origin) {
-            console.log(allowedOrigins.indexOf(origin));
-            callback(null, true)
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
         } else {
-            console.log(allowedOrigins.indexOf(origin));
             callback(new Error('Not allowed by CORS'));
         }
     },
-    optionsSuccessStatus: 200
-}
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // Allow credentials
+};
 
 export default corsOptions;
