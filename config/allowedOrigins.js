133757b57e1294afacc1b02bcd9696136cfae6a8
Matthew Mayer <grafixgeek@yahoo.com>
2024-02-21 20:54:06 -0500

    Added allowed origin for CORS

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'http://127.0.0.1:5173/backstage/',
     'http://127.0.0.1:5173/',
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'https://playhouseacademy.onrender.com/backstage/'
 ];
 
 export default allowedOrigins;
