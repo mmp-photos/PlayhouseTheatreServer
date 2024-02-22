421d7c9b3951e720d28dc96b3e9ab9c138b0bcd0
Matthew Mayer <grafixgeek@yahoo.com>
2024-02-22 14:40:40 -0500

    CORS updated

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,14 +4,15 @@ const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
+            console.log(`Found in allowedOrigins`)
         } else {
             callback(new Error('Not allowed by CORS'));
+            console.log(`NOT Found in allowedOrigins`)
         }
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow credentials
-    origin: ['https://playhouseacademy.onrender.com', 'https://playhouseacademy.onrender.com/backstage']
 };
 
 export default corsOptions;
