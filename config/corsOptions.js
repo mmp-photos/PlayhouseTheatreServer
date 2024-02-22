3cc6833591c0d142a33f2547e6dd4b2bc130e6e6
Matthew Mayer <grafixgeek@yahoo.com>
2024-02-22 10:54:29 -0500

    Turned CORS back on.

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,22 +1,17 @@
 import allowedOrigins from './allowedOrigins.js';
 
 const corsOptions = {
-    // origin: (origin, callback) => {
-    //     if (allowedOrigins.indexOf(origin) !== -1) {
-    //         callback(null, true);
-    //     } else {
-    //         callback(new Error('Not allowed by CORS'));
-    //     }
-    // },
-    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
-    // credentials: true, // Allow credentials
-    // origin: ['https://playhouseacademy.onrender.com', 'https://playhouseacademy.onrender.com/backstage']
-    
-    origin: "*", // Allow requests from any origin
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false, // Do not allow credentials
+    credentials: true, // Allow credentials
+    origin: ['https://playhouseacademy.onrender.com', 'https://playhouseacademy.onrender.com/backstage']
 };
 
 export default corsOptions;
